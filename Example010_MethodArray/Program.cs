d00ada14638f673b7132586a8a096b6d15740fa4
Maxim Sukhochev <suhochev_m@mail.ru>
2022-12-02 17:59:13 +0300

    added Ex10 Method Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 12, 93, 34, 45, 56, 67, 45, 78, 89 };
+
+int n = array.Length;
+int find = 45;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
